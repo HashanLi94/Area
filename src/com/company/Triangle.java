1287ea3cfa5fcc03802d30dacfaa9170e48c2907
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-02-14 22:07:37 +0530

    Create Triangle.java

---
+++ src/com/company/Triangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Triangle extends Polygen {
+    public Triangle(){
+
+
+    }
+
+    public double area(){
+        double a = width*height;
+        return a;
+    }
+}
