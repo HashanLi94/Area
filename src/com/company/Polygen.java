80ea73bbb906aad7a441644e1cca60d70d0391f0
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-02-14 22:07:34 +0530

    Create Polygen.java

---
+++ src/com/company/Polygen.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Polygen {
+
+    Scanner scanner = new Scanner(System.in);
+
+    int height = scanner.nextInt();
+
+
+    int width = scanner.nextInt();
+
+
+
+}
