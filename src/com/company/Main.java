81c71b5e30e45e3d79c8c320121b56c48ba4a9f8
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-02-14 22:07:30 +0530

    Create Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle tr = new Triangle();
+        System.out.println("The are of the triangle is "+ tr.area());
+
+//        Scanner scanner = new Scanner(in);
+//        System.out.println("Enter the name here");
+//        int age = scanner.nextInt();
+//        System.out.println("your age is " + age);
+    }
+}
